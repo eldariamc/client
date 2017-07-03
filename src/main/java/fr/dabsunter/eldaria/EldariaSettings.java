6b9701d4085364da19182cf60707036dfa6a9af0
Dabsunter <dav.nico@orange.fr>
2017-07-03 19:42:24 +0200

    Ajout des méthodes manquantes au système de settings

--- src/main/java/fr/dabsunter/eldaria/EldariaSettings.java
+++ src/main/java/fr/dabsunter/eldaria/EldariaSettings.java
@@ -13,6 +13,22 @@ public class EldariaSettings {
 
 	private static byte hudPreset = 1;
 
+	public static boolean getBooleanValue(Options option) {
+		switch (option) {
+
+			default:
+				throw new IllegalStateException(name(option) + " is not a Boolean option");
+		}
+	}
+
+	public static void setBooleanValue(Options option, boolean value) {
+		switch (option) {
+
+			default:
+				throw new IllegalStateException(name(option) + " is not a Boolean option");
+		}
+	}
+
 	public static byte getByteValue(Options option) {
 		switch (option) {
 			case HUD_PRESETS:
@@ -33,6 +49,102 @@ public class EldariaSettings {
 		}
 	}
 
+	public static short getShortValue(Options option) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a Short option");
+		}
+	}
+
+	public static void setShortValue(Options option, short value) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a Short option");
+		}
+	}
+
+	public static int getIntegerValue(Options option) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a Integer option");
+		}
+	}
+
+	public static void setIntegerValue(Options option, int value) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a Integer option");
+		}
+	}
+
+	public static long getLongValue(Options option) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a Long option");
+		}
+	}
+
+	public static void setLongValue(Options option, long value) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a Long option");
+		}
+	}
+
+	public static float getFloatValue(Options option) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a Float option");
+		}
+	}
+
+	public static void setFloatValue(Options option, float value) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a Float option");
+		}
+	}
+
+	public static double getDoubleValue(Options option) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a Double option");
+		}
+	}
+
+	public static void setDoubleValue(Options option, double value) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a Double option");
+		}
+	}
+
+	public static String getStringValue(Options option) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a String option");
+		}
+	}
+
+	public static void setStringValue(Options option, String value) {
+		switch (option) {
+
+			default:
+				throw new IllegalArgumentException(name(option) + " is not a String option");
+		}
+	}
+
 	public static void load(File root) {
 		optionsFile = new File(root, "optionsel.txt");
 
@@ -48,28 +160,28 @@ public class EldariaSettings {
 							try {
 								switch (option.getType()) {
 									case BOOLEAN:
-
+										setBooleanValue(option, Boolean.parseBoolean(args[1]));
 										break;
 									case BYTE:
 										setByteValue(option, Byte.parseByte(args[1]));
 										break;
 									case SHORT:
-
+										setShortValue(option, Short.parseShort(args[1]));
 										break;
 									case INTEGER:
-
+										setIntegerValue(option, Integer.parseInt(args[1]));
 										break;
 									case LONG:
-
+										setLongValue(option, Long.parseLong(args[1]));
 										break;
 									case FLOAT:
-
+										setFloatValue(option, Float.parseFloat(args[1]));
 										break;
 									case DOUBLE:
-
+										setDoubleValue(option, Double.parseDouble(args[1]));
 										break;
 									case STRING:
-
+										setStringValue(option, args[1]);
 								}
 							} catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
 								System.err.println(args[0] + " a été ignoré -> " + ex);
@@ -79,6 +191,7 @@ public class EldariaSettings {
 
 					}
 				}
+				reader.close();
 			} catch (IOException e) {
 				System.err.println("Impossible de charger les réglages Eldaria !");
 				e.printStackTrace();
@@ -98,28 +211,28 @@ public class EldariaSettings {
 				writer.print(':');
 				switch (option.getType()) {
 					case BOOLEAN:
-
+						writer.println(getBooleanValue(option));
 						break;
 					case BYTE:
 						writer.println(getByteValue(option));
 						break;
 					case SHORT:
-
+						writer.println(getShortValue(option));
 						break;
 					case INTEGER:
-
+						writer.println(getIntegerValue(option));
 						break;
 					case LONG:
-
+						writer.println(getLongValue(option));
 						break;
 					case FLOAT:
-
+						writer.println(getFloatValue(option));
 						break;
 					case DOUBLE:
-
+						writer.println(getDoubleValue(option));
 						break;
 					case STRING:
-
+						writer.println(getStringValue(option));
 				}
 			}
 
